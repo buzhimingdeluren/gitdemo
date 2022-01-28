4e7bbf96be584981a858200abc2bbef574747473
Yaokangxing <1154778101@qq.com>
2022-01-28 16:43:38 +0800

    hot-fix 分支合并测试

--- src/main/java/com/hdax/gitdemo/GitdemoApplication.java
+++ src/main/java/com/hdax/gitdemo/GitdemoApplication.java
@@ -9,7 +9,10 @@ public class GitdemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(GitdemoApplication.class, args);
         System.out.println("hellogit");
-        System.out.println("你好呀 git");
+        System.out.println("你好呀 git  第一个版本");
+        System.out.println("你好呀 git  第二个版本");
+        System.out.println("你好呀 git  第三个版本");
+        System.out.println("hot-fix 分支合并");
     }
 
 }
