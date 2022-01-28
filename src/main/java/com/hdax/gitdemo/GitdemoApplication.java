dc877fa647edc017ca44bd100669c138695f7dd3
Yaokangxing <1154778101@qq.com>
2022-01-28 16:05:16 +0800

    first  commit  by 姚康星

---
+++ src/main/java/com/hdax/gitdemo/GitdemoApplication.java
@@ -0,0 +1,15 @@
+package com.hdax.gitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitdemoApplication.class, args);
+        System.out.println("hellogit");
+        System.out.println("你好呀 git");
+    }
+
+}
